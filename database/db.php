4fa16ec084c98d9d93dd5c3a248c8409a2485f23
X-Jade <71828843+X-Jade@users.noreply.github.com>
2022-10-17 15:30:13 +0700

    Update db.php

--- database/db.php
+++ database/db.php
@@ -1,10 +1,36 @@
 <?php
 
 // $db_params = parse_ini_file( dirname(__FILE__).'/db_params.ini', false );
+require 'aws.phar';
+
+//Get the application environment parameters from the Parameter Store.
+
+$az = file_get_contents('http://169.254.169.254/latest/meta-data/placement/availability-zone');
+$region = substr($az, 0, -1);
+
+$ssm_client = new Aws\Ssm\SsmClient([
+    'version' => 'latest',
+    'region'  => $region
+]);
+
+$result = $ssm_client->GetParametersByPath(['Path' => '/guestbook']);
+
+$db_url = "";
+$db_name = "";
+$db_user = "";
+$db_password = "";
+
+foreach($result['Parameters'] as $p) {
+    if ($p['Name'] == '/guestbook/db-endpoint') $db_url = $p['Value'];
+    if ($p['Name'] == '/guestbook/db-name') $db_name = $p['Value'];
+    if ($p['Name'] == '/guestbook/db-user') $db_user = $p['Value'];
+    if ($p['Name'] == '/guestbook/db-pass') $db_password = $p['Value'];
+}
+
 
 try {
-    $conn = new mysqli('localhost', 'root', '', 'guestbook');
-	// $conn = new mysqli($db_params['host'], $db_params['user'], $db_params['password'], $db_params['dbname']);
+//     $conn = new mysqli('localhost', 'root', '', 'guestbook');
+	$conn = new mysqli($db_url, $db_user, $db_password, $db_name);
 } catch (Exception $e) {
 	echo 'Connection failed';
 }
